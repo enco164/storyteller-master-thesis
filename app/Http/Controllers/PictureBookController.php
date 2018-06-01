c9ba86180eeb89347a4e09966eeb18fe7fa20cc5
Uros Milenkovic <umilenko@calliduscloud.com>
2018-06-01 19:40:04 +0200

    feat(picture book): scaffolding backend for picture book

---
+++ app/Http/Controllers/PictureBookController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PictureBook;
+use Illuminate\Http\Request;
+
+class PictureBookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PictureBook  $pictureBook
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PictureBook $pictureBook)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PictureBook  $pictureBook
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PictureBook $pictureBook)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PictureBook  $pictureBook
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PictureBook $pictureBook)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PictureBook  $pictureBook
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PictureBook $pictureBook)
+    {
+        //
+    }
+}
---
+++ database/factories/PictureBookFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\PictureBook::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
